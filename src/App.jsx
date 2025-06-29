a9f66fe76c39490b77d88500d4c120f134cc092d
ThHungg <dth052k4@gmail.com>
2025-06-29 20:33:59 +0700

    Start 29/06

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-brandOrange font-custom">
+        Custom Tailwind styles!
+      </div>
+      <div className="font-bold">Custom Tailwind styles!</div>
+    </>
+  );
+}
+
+export default App;
